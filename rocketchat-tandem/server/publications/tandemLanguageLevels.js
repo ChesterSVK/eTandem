f7f3fa47ea50a98dc4f2882843a65e53878e6e8c
ChesterSVK <jozefcibik@gmail.com>
2019-03-13 15:21:36 +0200

    Remove duplicated directory

--- rocketchat-tandem/server/publications/tandemLanguageLevels.js
+++
@@ -1,8 +0,0 @@
-import {Meteor} from 'meteor/meteor';
-import TandemLanguageLevels from '../models/TandemLanguageLevels'
-
-Meteor.publish({
-	'tandemLanguageLevels'() {
-		return TandemLanguageLevels.findAll();
-	},
-});
